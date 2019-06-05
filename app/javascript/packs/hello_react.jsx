503b58062a9c1a90c6377943cc3b992b32ce255b
Christophe Eo Siang Ee <aragonlotr5@gmail.com>
2019-06-05 19:22:02 +0800

    ran yarn update and fixed the link to components/app

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -2,9 +2,10 @@
 // like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
 // of the page.
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+import App from '../components/app';
 
 const Hello = props => (
   <div>Hello {props.name}!</div>
@@ -20,7 +21,7 @@ Hello.propTypes = {
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Hello name="React" />,
+    <App />,
     document.body.appendChild(document.createElement('div')),
   )
 })
